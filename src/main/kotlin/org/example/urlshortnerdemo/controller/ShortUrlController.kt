a12ec3a217a63bec285ac97d032a9a846623dc52
Namkyu An <skarb7746@gmail.com>
2024-06-19 23:40:54 +0900

    feat: http caching 추가

--- src/main/kotlin/org/example/urlshortnerdemo/controller/ShortUrlController.kt
+++ src/main/kotlin/org/example/urlshortnerdemo/controller/ShortUrlController.kt
@@ -5,8 +5,10 @@ import org.example.urlshortnerdemo.controller.response.ShortLinkResponse
 import org.example.urlshortnerdemo.exceptions.NotFoundException
 import org.example.urlshortnerdemo.model.ShortUrl
 import org.example.urlshortnerdemo.service.ShortUrlService
+import org.springframework.http.CacheControl
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/short-links")
@@ -36,6 +38,6 @@ class ShortUrlController(val service: ShortUrlService) {
     fun redirect(@PathVariable shortUrlId: String): ResponseEntity<Void> {
         val shortUrl: ShortUrl = service.getShortUrl(shortUrlId) ?: throw NotFoundException()
 
-        return ResponseEntity.status(302).header("Location", shortUrl.originalUrl).build()
+        return ResponseEntity.status(302).cacheControl(CacheControl.maxAge(30, TimeUnit.MINUTES).noTransform()).header("Location", shortUrl.originalUrl).build()
     }
 }
